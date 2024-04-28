c9edb8ff38971cf0fedf283f54b6f841e264b193
charbel ayrouth <ayrouthcharbel@gmail.com>
2024-04-28 21:35:46 +0300

    upload images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,36 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 // every time we made a change in the DB, the page will be updated on the next visit
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
-
   return (
-    <main className="">
-      <div className="flex flex-wrap gap-4">
-        {[...images, ...images, ...images].map((image, index) => (
-          <div className="flex w-48 flex-col" key={image.id + "-" + index}>
-            <img src={image.url} alt={image.url} />
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
+    <div className="flex flex-wrap gap-4">
+      {images.map((image) => (
+        <div className="flex h-36 w-48 flex-col" key={image.id}>
+          <img src={image.url} alt={image.url} />
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default async function HomePage() {
+  return (
+    <main>
+      <SignedOut>
+        <div className="h-full w-full text-center text-2xl">
+          Please sign in above
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
