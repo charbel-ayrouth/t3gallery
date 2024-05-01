5170d00a59a9fb523e832f992dd2d8670eb12e41
charbel ayrouth <ayrouthcharbel@gmail.com>
2024-05-01 15:47:17 +0300

    Image from Nextjs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { getMyImages } from "~/server/queries";
+import Image from "next/image";
 
 // every time we made a change in the DB, the page will be updated on the next visit
 export const dynamic = "force-dynamic";
@@ -8,10 +9,16 @@ async function Images() {
   const images = await getMyImages();
 
   return (
-    <div className="flex flex-wrap gap-4">
+    <div className="flex flex-wrap justify-center gap-4">
       {images.map((image) => (
-        <div className="flex h-36 w-48 flex-col" key={image.id}>
-          <img src={image.url} alt={image.url} />
+        <div className="flex h-48 w-48 flex-col" key={image.id}>
+          <Image
+            src={image.url}
+            alt={image.name}
+            style={{ objectFit: "contain" }}
+            width={192}
+            height={192}
+          />
           <div>{image.name}</div>
         </div>
       ))}
